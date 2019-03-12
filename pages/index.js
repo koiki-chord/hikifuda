5ae02f643ff5400a0c86b072f17ed8d7fb9a5c84
taka66 <taka66.iphone@gmail.com>
2019-03-12 22:45:08 +0900

    Fetch with mock api response.

--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,35 @@
 import Layout from '../components/Layout.js'
 import styled from 'styled-components'
+import Link from 'next/link'
+import fetch from 'isomorphic-unfetch'
 
-const Title = styled.p`
+const Title = styled.h1`
   color: red;
   font-size: 50px;
 `
 
-export default () => (
+const Index = props => (
   <Layout>
-    <Title>Hello Next.js</Title>
+    <Title>Community Detail</Title>
+    <Link
+      as={`/p/${props.community.id}`}
+      href={`/post?id=${props.community.id}`}
+    >
+      <a>{props.community.name}</a>
+    </Link>
   </Layout>
 )
+
+Index.getInitialProps = async function() {
+  // TODO mock api response
+  const res = await fetch('http://www.mocky.io/v2/5c87b65d320000af133bd443')
+  const data = await res.json()
+
+  console.log('response', data)
+
+  return {
+    community: data
+  }
+}
+
+export default Index
